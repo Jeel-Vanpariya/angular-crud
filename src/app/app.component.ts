9d7c4239f187a14e876084605f60fb1f79fbbed4
Jeel Vanpariya <70009506+Jeel-Vanpariya@users.noreply.github.com>
2021-04-18 22:28:21 +0530

    Add files via upload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component, VERSION } from '@angular/core';
+import { Component } from '@angular/core';
+import { AuthService } from './auth.service';
 
 @Component({
-  selector: 'my-app',
+  selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+export class AppComponent {
+  title = 'Auth';
+
+  constructor(public _authService:AuthService){
+
+  }
+
+  logout(){
+    this._authService.logout();
+  }
+
 }
